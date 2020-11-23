6523b8d2c566a4d16edafccfd6325d0417abd514
Lingrode <illia050@ukr.net>
2020-11-23 18:49:21 +0200

    added new design and video-popup

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,34 @@ $(function () {
     autoplaySpeed: 4000
   })
 
-  var mixer = mixitup('.products__items');
+  
+  // var mixerOne = mixitup('.products__items');
+
+  var mixerOne = mixitup('.products__items', {
+    selectors: {
+      control: '.products__filter'
+    }
+  }),
+
+  // var mixerTwo = mixitup('.new-design__items');
+
+  var mixerTwo = mixitup('.new-design__items', {
+    selectors: {
+      control: '.new-design__filter'
+    }
+  });
 });
+
+// $(document).ready(function () {
+//   $('.products__items').mixitup({
+//     selectors: {
+//       filter: '.products__filter-btn'
+//     }
+//   });
+
+//   $('.new-design__items').mixitup({
+//     selectors: {
+//       filter: '.new-design__filter-btn'
+//     }
+//   });
+// })
