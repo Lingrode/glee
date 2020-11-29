59fafbddf9ce9c65627bbd294bdd80bb7b5b2448
Lingrode <illia050@ukr.net>
2020-11-29 14:55:19 +0200

    added login page, terms & conditions page, partners and articles

--- app/js/main.js
+++ app/js/main.js
@@ -7,34 +7,20 @@ $(function () {
     autoplaySpeed: 4000
   })
 
-  
-  // var mixerOne = mixitup('.products__items');
 
-  var mixerOne = mixitup('.products__items', {
-    selectors: {
-      control: '.products__filter'
-    }
-  }),
+  var mixerOne = mixitup('.products__items');
 
-  // var mixerTwo = mixitup('.new-design__items');
+  // var myName = mixitup('.products__items', {
+  //   selectors: {
+  //     control: '.products__filter'
+  //   }
+  // }),
 
-  var mixerTwo = mixitup('.new-design__items', {
-    selectors: {
-      control: '.new-design__filter'
-    }
-  });
+  var mixerTwo = mixitup('.new-design__items');
+
+  // var mixerTwo = mixitup('.new-design__items', {
+  //   selectors: {
+  //     control: '.new-design__filter'
+  //   }
+  // });
 });
-
-// $(document).ready(function () {
-//   $('.products__items').mixitup({
-//     selectors: {
-//       filter: '.products__filter-btn'
-//     }
-//   });
-
-//   $('.new-design__items').mixitup({
-//     selectors: {
-//       filter: '.new-design__filter-btn'
-//     }
-//   });
-// })
